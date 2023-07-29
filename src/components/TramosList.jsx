bcc9eb69b1d340d249d04bf0530016a27501b170
Steveen <steveena.dominguezb@gmail.com>
2023-07-29 18:03:48 -0500

    mostrando datos json en TramosList

--- src/components/TramosList.jsx
+++ src/components/TramosList.jsx
@@ -1,6 +1,48 @@
+import { useEffect } from "react";
+import { useState } from "react"
+
 function TramosList() {
+
+    const [tramos, setTramos] = useState(null);
+
+    const getTramos = async () => {
+        const endpoint = "http://localhost:4000/tramos";
+        const requestData = {
+            fechainicial: "2010-01-01",
+            fechafinal: "2010-01-30"
+        };
+
+        try {
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestData),
+            });
+
+            if (!response.ok) {
+                throw new Error('Response was not ok');
+            }
+
+            const responseData = await response.json();
+            setTramos(responseData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setTramos(null);
+        }
+    };
+    useEffect(() => {
+        getTramos();
+    }, []);
+
     return (
-        <div>TramosList</div>
+        <>
+            <div>TramosList</div>
+            <div>
+                {JSON.stringify(tramos)}
+            </div>
+        </>
     )
 }
 
